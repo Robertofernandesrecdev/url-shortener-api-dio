bc3b8b4f1411b0751aa645348bf48cbb26ac1845
Roberto Fernandes <robertofernandes199@hotmail.com>
2022-03-23 19:14:21 -0300

    encurtador de url DIO

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: 'mongodb+srv://robertofernandes199:<password>@project-dio.5kb0y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+    API_URL: "http://localhost:5000",
+
+    MONGO_CONNECTION:
+        'mongodb+srv://robertofernandes199:<password>@project-dio.5kb0y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
